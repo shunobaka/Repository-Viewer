edc4cf3351c2e2cc444d0198d334a399eb5fb57d
Veselin Minev <vesselin.minev6@gmail.com>
2020-08-23 16:34:28 +0200

    Created redux store and attached it to App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Custom components
 import Navigation from './components/layout/Navigation';
 import Landing from './components/layout/Landing';
 import Routes from './components/routes/Routes';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+// Styling
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route component={Routes} />
-        </Switch>
-      </Fragment>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
