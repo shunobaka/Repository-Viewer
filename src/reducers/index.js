d476571ba1d38ff0b5ef9a1fb0b2f49e3ebfc932
Veselin Minev <vesselin.minev6@gmail.com>
2020-08-24 16:06:55 +0200

    Added repository reducer to combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import repository from './repository';
 import alert from './alert';
 
-export default combineReducers({ user, alert });
+export default combineReducers({ user, repository, alert });
