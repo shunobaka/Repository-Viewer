b2541d63320f7d25771b12894c1c1796ba7201c9
Veselin Minev <vesselin.minev6@gmail.com>
2020-08-25 15:50:12 +0200

    Added documentation to routes component

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,9 +1,15 @@
+/**
+ * @fileoverview Defines a Routes react component that renders a react router.
+ */
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from '../layout/NotFound';
 import Search from '../search/Search';
 import User from '../user/User';
 
+/**
+ * Routes react component that renders a react router specifying accessible routes.
+ */
 const Routes = () => {
   return (
     <Switch>
