5b93f69a805fc7f5d2d0116e6d755d9ec29c2684
Veselin Minev <vesselin.minev6@gmail.com>
2020-08-25 15:32:44 +0200

    Added documentation to utils

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -1,5 +1,12 @@
+/**
+ * @fileoverview Defines an axios object used to make http requests to the github API.
+ */
 import axios from 'axios';
 
+/**
+ * The axios object used to make http requests to the github API.
+ * @export
+ */
 const githubApi = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,3 +1,12 @@
+/**
+ * @fileoverview Defines a function that generates a UUIDv4.
+ */
+
+/**
+ * Generates a UUIDv4 and returns it as a string.
+ * @return {string} The generated UUIDv4
+ * @export
+ */
 export default () => {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
     (
