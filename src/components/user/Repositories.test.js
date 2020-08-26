bb1846a9ab934e8518bd278da2a3e41938b48790
Veselin Minev <vesselin.minev6@gmail.com>
2020-08-26 02:25:31 +0200

    Implemented snapshot tests for user components

---
+++ src/components/user/Repositories.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Repositories from './Repositories';
+
+describe('repositories snapshots', () => {
+  const mockRepos = [
+    {
+      name: 'Test',
+      id: 0,
+      description: 'Description',
+    },
+    {
+      name: 'Test2',
+      id: 1,
+    },
+  ];
+
+  it('mutliple repositories renders correctly', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Repositories repositories={mockRepos} />
+        </BrowserRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('single repository renders correctly', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Repositories repositories={[mockRepos[0]]} />
+        </BrowserRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('no repositories renders correctly', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Repositories repositories={[]} />
+        </BrowserRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/user/RepositoryItem.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import RepositoryItem from './RepositoryItem';
+
+describe('repositoryItem snapshots', () => {
+  const mockRepo = {
+    name: 'Test',
+    id: 0,
+  };
+
+  it('repository renders correctly', () => {
+    const index = 0;
+
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <RepositoryItem repository={mockRepo} index={index} />
+        </BrowserRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
